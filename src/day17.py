43313e2577c0da7309fdae6daf09162d7972db59
Jacob Parish <jacobsparish@gmail.com>
2022-12-11 18:03:28 -0500

    Make day 17 less repetitious

--- src/day17.py
+++ src/day17.py
@@ -3,7 +3,9 @@ import hashlib
 import utils
 
 
-def get_neighbors_1(seed: str, path: str, x: int, y: int):
+def get_neighbors(seed: str, path: str, x: int, y: int):
+    if (x, y) == (3, 3):
+        return
     u, d, l, r = hashlib.md5((seed + path).encode()).hexdigest()[:4]
     if y > 0 and u in "bcdef":
         yield (path + "U", x, y - 1)
@@ -15,26 +17,6 @@ def get_neighbors_1(seed: str, path: str, x: int, y: int):
         yield (path + "R", x + 1, y)
 
 
-dists1 = utils.shortest_paths(
-    ("", 0, 0),
-    lambda v: get_neighbors_1(data, *v),
-    stop_condition=lambda dists: any(node[1:] == (3, 3) for node in dists),
-)
-p1 = next(node[0] for node in dists1 if node[1:] == (3, 3))
-
-
-def get_neighbors_2(seed: str, path: str, x: int, y: int):
-    if (x, y) != (3, 3):
-        u, d, l, r = hashlib.md5((seed + path).encode()).hexdigest()[:4]
-        if y > 0 and u in "bcdef":
-            yield (path + "U", x, y - 1)
-        if y < 3 and d in "bcdef":
-            yield (path + "D", x, y + 1)
-        if x > 0 and l in "bcdef":
-            yield (path + "L", x - 1, y)
-        if x < 3 and r in "bcdef":
-            yield (path + "R", x + 1, y)
-
-
-dists2 = utils.shortest_paths(("", 0, 0), lambda v: get_neighbors_2(data, *v))
-p2 = max(dists2.values())
+dists = utils.shortest_paths(("", 0, 0), lambda v: get_neighbors(data, *v))
+p1 = min((v for v in dists if v[1:] == (3, 3)), key=dists.get)[0]
+p2 = max(dists.values())
